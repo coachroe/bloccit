deea4fba3db9be3fd7927a039c9d229ef07de0a5
coachroe <coachroe10@gmail.com>
2018-10-05 09:40:58 -0500

    Assignment 8 Seeding Data: Completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 require 'random_data'
+include RandomData
 
  # Create Posts
  50.times do
@@ -20,6 +21,13 @@ require 'random_data'
      body: RandomData.random_paragraph
    )
  end
+
+puts "#{Post.count}"
+  Post.find_or_create_by(title: "A unique title", body: "A unique body")
+puts "#{Post.count}"
+puts "#{Comment.count}"
+  Comment.find_or_create_by(post: posts.first, body: "A unique body")
+puts "#{Comment.count}"
 
  puts "Seed finished"
  puts "#{Post.count} posts created"
