75bf3fcc74d4f154749b18b82296be73df950f46
coachroe <coachroe10@gmail.com>
2018-10-08 10:08:37 -0500

    Master Clean Up

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -4,7 +4,9 @@ RSpec.describe Answer, type: :model do
   let(:question) { Question.create!(title: "New Question Title", body: "New Question Body") }
   let(:answer) { Answer.create!(body: 'Answer Body', question: question) }
 
-  it "should respond to body" do
-    expect(answer).to respond_to(:body)
+  describe "attributes" do
+    it "should respond to body" do
+      expect(answer).to respond_to(:body)
+    end
   end
 end
