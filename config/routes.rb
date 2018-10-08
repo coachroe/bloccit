71fa9fd75cf70ffb75b28318b30d58f4e4102cf5
coachroe <coachroe10@gmail.com>
2018-10-08 09:31:46 -0500

    Assignment 3 to 5 cleanup

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   resources :questions
 
   get 'about' => 'welcome#about'
+  get 'contact' => 'welcome#contact'
+  get 'FAQ' => 'welcome#FAQ'
 
   root 'welcome#index'
 end
