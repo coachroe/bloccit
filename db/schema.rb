e5e3805c371db704ac3ca386b3650956dd3e1f71
coachroe <coachroe10@gmail.com>
2018-10-08 09:52:52 -0500

    Assignment 6 to 9 cleanup

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_05_170422) do
+ActiveRecord::Schema.define(version: 2018_10_08_144410) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,15 @@ ActiveRecord::Schema.define(version: 2018_10_05_170422) do
     t.integer "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "answers", force: :cascade do |t|
+    t.integer "question_id"
+    t.string "body"
+    t.string "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["question_id"], name: "index_answers_on_question_id"
   end
 
   create_table "comments", force: :cascade do |t|
