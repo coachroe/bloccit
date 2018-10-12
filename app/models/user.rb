d9ec439e9628b676a8785a0c0f3a401b420c7126
coachroe <coachroe10@gmail.com>
2018-10-12 14:26:56 -0500

    Checkpoint 12 Public Profiles: Completed

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,6 @@ class User < ApplicationRecord
 
   before_save { self.email = email.downcase if email.present? }
   before_save { self.role ||= :member }
-  before_save :format_name
 
   validates :name, length: { minimum: 1, maximum: 100 }, presence: true
   validates :password, presence: true, length: { minimum: 6 }, if: -> { password_digest.nil? }
@@ -17,18 +16,12 @@ class User < ApplicationRecord
 
   enum role: [:member, :admin]
 
-  def format_name
-    if name
-      name_array = []
-      name.split.each do |name_part|
-      name_array << name_part.capitalize
-    end
-
-    self.name = name_array.join(" ")
-    end
-  end
-
   def favorite_for(post)
     favorites.where(post_id: post.id).first
+  end
+
+  def avatar_url(size)
+    gravatar_id = Digest::MD5::hexdigest(self.email).downcase
+    "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}"
   end
 end
